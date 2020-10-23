b31aace13b45e2873f9fd18303767182702bce23
Hassan Ateeq <hassanateeq20@gmail.com>
2020-10-23 23:57:53 +0500

    Passed

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+// Template Litrels
+
+// var a = 5;
+
+// var t = `https://www.randomuser.me/api/${a}`;
+
+
+
+// var data = "String" + a;
+
+
+
+// Spread Operator
+
+    // var array = [];
+
